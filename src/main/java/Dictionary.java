acfe5b8a9b5e002bfee3fe054195cdfe6263edd3
ndt20020213 <20020213@vnu.edu.vn>
2021-09-25 16:55:35 +0700

    da xoa

--- src/main/java/Dictionary.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public class Dictionary {
-    public ArrayList<Word> words = new ArrayList<Word>();
-}
--- src/main/java/tudien.java
+++
@@ -1,3 +0,0 @@
-public class tudien {
-    for vjksdnvjkfvnjnsv
-}
